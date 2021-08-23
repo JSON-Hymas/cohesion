5c66b1e26defe4da6165d2d09013bc515fa0a63c
Jayson Hymas <json.hymas.dev@outlook.com>
2021-08-22 19:10:40 -0500

    Skeleton controller, initial model and enum created

---
+++ cohesion/Controllers/MaintenanceServiceController.cs
@@ -0,0 +1,22 @@
+﻿using cohesion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cohesion.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MaintenanceServiceController : ControllerBase
+    {
+        public IEnumerable<MaintenanceServiceModel> serviceRequests { get; set; }
+
+        [HttpGet]
+        public IEnumerable<MaintenanceServiceModel> Get()
+        {
+            return serviceRequests;
+        }
+    }
+}
---
+++ cohesion/Enums/CurrentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cohesion.Enums
+{
+    public enum CurrentStatus
+    {
+        NotApplicable,
+        Created,
+        InProgress,
+        Complete,
+        Canceled
+    }
+}
